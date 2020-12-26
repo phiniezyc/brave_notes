92de80defa7789ccf7b3c61b9eb9cd5cf43c3d23
Chance Phiniezy <phiniezyc@gmail.com>
2020-12-26 04:37:53 -0500

    Change credentials file

--- firebase.js
+++ firebase.js
@@ -1,13 +1,13 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
-  measurementId: '',
+  apiKey: 'AIzaSyAXUJuaD2h0pvjyEz6JOijYpMq2WJaAb68',
+  authDomain: 'brave-notes.firebaseapp.com',
+  projectId: 'brave-notes',
+  storageBucket: 'brave-notes.appspot.com',
+  messagingSenderId: '580783597062',
+  appId: '1:580783597062:web:2c9abf3d4f778ef8260587',
+  measurementId: 'G-G2LSJJRDFL',
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
